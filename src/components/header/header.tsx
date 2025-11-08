96eb1fb7fa7470442fc7464ca811cbbce9ced61e
Артем <apaksha@yandex.ru>
2025-11-08 23:58:21 +0300

    add header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { HeaderContainer, HeaderText } from "./header.styled"
+
+export const Header = () => {
+    return (
+        <HeaderContainer>
+            <HeaderText>Заглушка под будущее</HeaderText>
+            <HeaderText>BRAINWAVE HUB</HeaderText>
+            <HeaderText>Заглушка под будущее</HeaderText>
+        </HeaderContainer>
+    )
+}
